5dbd87d79ae61e96689af66fec184952544b993e
Yiran <yiran.xie@msn.com>
2014-04-08 14:36:55 +0800

    ver1.0 bug fixed

--- GitTest/GitTest/features.cpp
+++ GitTest/GitTest/features.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <assert.h>
 #include "features.h"
 
 
 //Ver 1.0
 int GetSum(int *input, int length)
 {
+	assert(input != NULL && length > 0);
 	int sum = 0;
 	for(int i = 0 ; i < length ; i ++)
 	{
